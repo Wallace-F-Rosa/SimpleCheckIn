674916b1b9e5ab355e418379bea56c7a2c3ec0ba
wallace-f-rosa <wferancini@gmail.com>
2020-12-13 15:38:46 -0300

    Métodos para recuperar uma Categoria especifica

--- app/src/main/java/ferancini/app/simplecheckin/Categoria.java
+++ app/src/main/java/ferancini/app/simplecheckin/Categoria.java
@@ -1,6 +1,7 @@
 package ferancini.app.simplecheckin;
 
 import android.database.Cursor;
+import android.util.Log;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -13,7 +14,33 @@ public class Categoria implements Serializable {
     private static List<Categoria> listCategoria;
 
     public Categoria(int idCategoria, String nome) {
+        this.idCategoria = idCategoria;
         this.nome = nome;
+    }
+
+    public static Categoria getCategoria(String nome){
+        Cursor cCat = BancoDadosSingleton.getInstance().buscar("Categoria",new String[]{"idCategoria","nome"},"nome='"+nome+"'","");
+
+        Categoria cat = null;
+        while (cCat.moveToNext()){
+            int colId = cCat.getColumnIndex("idCategoria");
+            int colNome = cCat.getColumnIndex("nome");
+            cat = new Categoria(cCat.getInt(colId), cCat.getString(colNome));
+        }
+        Log.i("CATEGORIA","Categoria pesquisada id="+cat.getIdCategoria()+" nome="+cat.getNome());
+        return cat;
+    }
+
+    public static Categoria getCategoria(int idCategoria){
+        Cursor cCat = BancoDadosSingleton.getInstance().buscar("Categoria",new String[]{"idCategoria","nome"},"idCategoria="+idCategoria,"");
+
+        Categoria cat = null;
+        while (cCat.moveToNext()){
+            int colId = cCat.getColumnIndex("idCategoria");
+            int colNome = cCat.getColumnIndex("nome");
+            cat = new Categoria(cCat.getInt(colId), cCat.getString(colNome));
+        }
+        return cat;
     }
 
     public static List<Categoria> getCategorias(){
